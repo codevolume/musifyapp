7224b00beceea6442a3f29000a710ace7de4a1ee
Marinka Szilveszter <szilvesztermarinka4@gmail.com>
2024-01-03 18:01:33 +0100

    even idk

--- components/Item.js
+++ components/Item.js
@@ -1,26 +1,30 @@
-import { View, Text, Pressable, Linking } from "react-native";
+import { View, Text, Pressable, Linking, Image } from "react-native";
 import React, { useContext } from "react";
 import { Player } from "../PlayerContext";
 
 const Item = ({ item, onPress, isPlaying }) => {
     const { currentTrack, setCurrentTrack } = useContext(Player);
-    const handlePress =  () => {
+    const handlePress = () => {
         setCurrentTrack(item);
-        console.log(item)
-        onPress(item)
-    }
+        console.log(item);
+        onPress(item);
+    };
 
+    const isCurrentTrack = currentTrack && currentTrack.title == item.title;
 
     return (
         <Pressable onPress={handlePress}>
-            <View style={{ flexDirection: "row", gap: 11, alignItems: "center", paddingVertical: 6, width: 260 }}>
-                <View style={{ backgroundColor: "#1e40af", width: 48, height: 48, borderRadius: 8, justifyContent: "center", alignItems: "center" }}>
-                    <Text style={{ color: "#60a5fa", fontSize: 20, fontWeight: "bold" }}>{item.title.charAt(0)}</Text>
+            <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
+                <View style={{ flexDirection: "row", gap: 11, alignItems: "center", paddingVertical: 6, width: "100%" }}>
+                    <View style={{ backgroundColor: "#1e40af", width: 48, height: 48, borderRadius: 8, justifyContent: "center", alignItems: "center" }}>
+                        <Text style={{ color: "#60a5fa", fontSize: 20, fontWeight: "bold" }}>{/* {item.title.charAt(0)} */}S</Text>
+                    </View>
+                    <View>
+                        <Text style={{ fontSize: 16, fontWeight: "500", color: isCurrentTrack ? "green" : "#202020" }}>{item.title}</Text>
+                        <Text style={{ fontSize: 12, color: "#646464" }}>{item.singer}</Text>
+                    </View>
                 </View>
-                <View>
-                    <Text style={{ fontSize: 16, fontWeight: "500", color: "#202020", width: 180 }} numberOfLines={1}>{item.title}</Text>
-                    <Text style={{ fontSize: 12, color: "#646464" }}>{item.singer}</Text>
-                </View>
+                <View>{isCurrentTrack ? <Image source={require("../assets/eq.gif")} style={{ height: 20, width: 20 }} /> : null}</View>
             </View>
         </Pressable>
     );
--- screens/app/HomeScreen.js
+++ screens/app/HomeScreen.js
@@ -1,6 +1,6 @@
 import { SafeAreaView, View, Text, FlatList, Platform, Pressable, Modal } from "react-native";
-import { ScrollView } from "react-native-virtualized-view";
 import React, { useContext, useState } from "react";
+import { ScrollView } from "react-native-virtualized-view";
 import Item from "../../components/Item";
 import CategoryItem from "../../components/CategoryItem";
 import { Player } from "../../PlayerContext";
@@ -29,32 +29,33 @@ const HomeScreen = () => {
                         </Pressable>
                     </View>
 
-                    <ScrollView style={{ height: "100%", backgroundColor: "#f9f9f9" }}>
-                        <View style={{ paddingTop: 25, gap: 16 }}>
-                            <FlatList contentContainerStyle={{ paddingHorizontal: 25 }} horizontal={true} showsHorizontalScrollIndicator={false} data={category} renderItem={({ categoryItem }) => <CategoryItem item={categoryItem} />} />
-                        </View>
-                        <View style={{ paddingTop: 35, paddingHorizontal: 25 }}>
-                            <View style={{ paddingBottom: 18 }}>
-                                <Text style={{ fontSize: 20, color: "#202020", fontWeight: "bold" }}>Quick Play</Text>
+                    <View style={{ height: "100%", backgroundColor: "#f9f9f9" }}>
+                        <ScrollView style={{ flex: 1, marginBottom: currentTrack ? 285 : 200 }} showsVerticalScrollIndicator={false}>
+                            <View style={{ paddingTop: 25, gap: 16 }}>
+                                <FlatList contentContainerStyle={{ paddingHorizontal: 25 }} horizontal={true} keyExtractor={(item, index) => index.toString()} showsHorizontalScrollIndicator={false} data={category} renderItem={({ categoryItem }) => <CategoryItem item={categoryItem} />} />
                             </View>
+                            <View style={{ paddingTop: 35, paddingHorizontal: 25 }}>
+                                <View style={{ paddingBottom: 18 }}>
+                                    <Text style={{ fontSize: 20, color: "#202020", fontWeight: "bold" }}>Quick Play</Text>
+                                </View>
 
-                            <FlatList numColumns={2} showsHorizontalScrollIndicator={false} data={Data} renderItem={({ item }) => <Item item={item} onPress={play} isPlaying={item === currentTrack} />} />
-                        </View>
-
-                        <View style={{ paddingTop: 35, paddingHorizontal: 25 }}>
-                            <View style={{ paddingBottom: 18 }}>
-                                <Text style={{ fontSize: 20, color: "#202020", fontWeight: "bold" }}>For You</Text>
+                                <FlatList showsHorizontalScrollIndicator={false} data={Data} keyExtractor={(item, index) => index.toString()} renderItem={({ item }) => <Item item={item} onPress={play} isPlaying={item === currentTrack} />} />
                             </View>
-                            <View></View>
-                        </View>
-                    </ScrollView>
+                            <View style={{ paddingTop: 35, paddingHorizontal: 25 }}>
+                                <View style={{ paddingBottom: 18 }}>
+                                    <Text style={{ fontSize: 20, color: "#202020", fontWeight: "bold" }}>For You</Text>
+                                </View>
+                                <FlatList showsHorizontalScrollIndicator={false} data={Data} keyExtractor={(item, index) => index.toString()} renderItem={({ item }) => <Item item={item} onPress={play} isPlaying={item === currentTrack} />} />
+                            </View>
+                        </ScrollView>
+                    </View>
                 </SafeAreaView>
             </View>
             <View>
                 <Modal visible={settingModal} onRequestClose={() => setSettingModal(false)} animationType="slide" presentationStyle="fullScreen">
-                    <View style={{ flex: 1, backgroundColor: "#f9f9f9" }}>
+                    <View style={{ flex: 1, backgroundColor: "#f0f0f0" }}>
                         <SafeAreaView>
-                            <View style={{ alignItems: "flex-end", paddingHorizontal: 25 }}>
+                            <View style={{ alignItems: "flex-end", paddingHorizontal: 25, paddingTop: 20 }}>
                                 <Pressable onPress={() => setSettingModal(false)}>
                                     <Svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
                                         <Path d="M18 6L6 18" stroke="#33363F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -68,23 +69,82 @@ const HomeScreen = () => {
                                 </View>
                                 <Text style={{ fontSize: 24, fontWeight: "bold", color: "#202020", paddingTop: 20 }}>Szilveszter</Text>
                             </View>
-                            <View style={{ paddingHorizontal: 25, paddingTop: 40 }}>
-                                <Pressable>
-                                    <View style={{ backgroundColor: "white", width: "100%", paddingVertical: 16, paddingHorizontal: 16, borderRadius: 6, flexDirection: "row", alignItems: "center", gap: 16}}>
-                                        <Svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                            <Path
-                                                d="M7 7.13193V6.61204C7 4.46614 7 3.3932 7.6896 2.79511C8.37919 2.19703 9.44136 2.34877 11.5657 2.65224L15.8485 3.26408C18.3047 3.61495 19.5327 3.79039 20.2664 4.63628C21 5.48217 21 6.72271 21 9.20377V14.7962C21 17.2773 21 18.5178 20.2664 19.3637C19.5327 20.2096 18.3047 20.385 15.8485 20.7359L11.5657 21.3478C9.44136 21.6512 8.37919 21.803 7.6896 21.2049C7 20.6068 7 19.5339 7 17.388V17.066"
-                                                stroke="red"
-                                                stroke-width="2"
-                                            />
-                                            <Path
-                                                d="M16 12L16.7809 11.3753L17.2806 12L16.7809 12.6247L16 12ZM4 13C3.44771 13 3 12.5523 3 12C3 11.4477 3.44771 11 4 11V13ZM12.7809 6.3753L16.7809 11.3753L15.2191 12.6247L11.2191 7.6247L12.7809 6.3753ZM16.7809 12.6247L12.7809 17.6247L11.2191 16.3753L15.2191 11.3753L16.7809 12.6247ZM16 13H4V11H16V13Z"
-                                                fill="red"
-                                            />
-                                        </Svg>
-                                        <Text style={{ fontSize: 16, fontWeight: "bold", color: "red" }}>Kijelentkezés</Text>
+
+                            <View style={{ paddingHorizontal: 25, paddingTop: 40, gap: 35 }}>
+                                <View style={{ gap: 18 }}>
+                                    <Text style={{ fontSize: 20, fontWeight: "bold", color: "#202020" }}>Általános</Text>
+                                    <View style={{ gap: 18 }}>
+                                        <Pressable>
+                                            <View style={{ backgroundColor: "white", width: "100%", paddingVertical: 16, paddingHorizontal: 16, borderRadius: 6, flexDirection: "row", alignItems: "center", gap: 16 }}>
+                                                <Svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                    <Path
+                                                        d="M7 7.13193V6.61204C7 4.46614 7 3.3932 7.6896 2.79511C8.37919 2.19703 9.44136 2.34877 11.5657 2.65224L15.8485 3.26408C18.3047 3.61495 19.5327 3.79039 20.2664 4.63628C21 5.48217 21 6.72271 21 9.20377V14.7962C21 17.2773 21 18.5178 20.2664 19.3637C19.5327 20.2096 18.3047 20.385 15.8485 20.7359L11.5657 21.3478C9.44136 21.6512 8.37919 21.803 7.6896 21.2049C7 20.6068 7 19.5339 7 17.388V17.066"
+                                                        stroke="red"
+                                                        stroke-width="2"
+                                                    />
+                                                    <Path
+                                                        d="M16 12L16.7809 11.3753L17.2806 12L16.7809 12.6247L16 12ZM4 13C3.44771 13 3 12.5523 3 12C3 11.4477 3.44771 11 4 11V13ZM12.7809 6.3753L16.7809 11.3753L15.2191 12.6247L11.2191 7.6247L12.7809 6.3753ZM16.7809 12.6247L12.7809 17.6247L11.2191 16.3753L15.2191 11.3753L16.7809 12.6247ZM16 13H4V11H16V13Z"
+                                                        fill="red"
+                                                    />
+                                                </Svg>
+                                                <Text style={{ fontSize: 16, fontWeight: "bold", color: "red" }}>Kijelentkezés</Text>
+                                            </View>
+                                        </Pressable>
+                                        <Pressable>
+                                            <View style={{ backgroundColor: "white", width: "100%", paddingVertical: 16, paddingHorizontal: 16, borderRadius: 6, flexDirection: "row", alignItems: "center", gap: 16 }}>
+                                                <Svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                    <Path
+                                                        d="M7 7.13193V6.61204C7 4.46614 7 3.3932 7.6896 2.79511C8.37919 2.19703 9.44136 2.34877 11.5657 2.65224L15.8485 3.26408C18.3047 3.61495 19.5327 3.79039 20.2664 4.63628C21 5.48217 21 6.72271 21 9.20377V14.7962C21 17.2773 21 18.5178 20.2664 19.3637C19.5327 20.2096 18.3047 20.385 15.8485 20.7359L11.5657 21.3478C9.44136 21.6512 8.37919 21.803 7.6896 21.2049C7 20.6068 7 19.5339 7 17.388V17.066"
+                                                        stroke="red"
+                                                        stroke-width="2"
+                                                    />
+                                                    <Path
+                                                        d="M16 12L16.7809 11.3753L17.2806 12L16.7809 12.6247L16 12ZM4 13C3.44771 13 3 12.5523 3 12C3 11.4477 3.44771 11 4 11V13ZM12.7809 6.3753L16.7809 11.3753L15.2191 12.6247L11.2191 7.6247L12.7809 6.3753ZM16.7809 12.6247L12.7809 17.6247L11.2191 16.3753L15.2191 11.3753L16.7809 12.6247ZM16 13H4V11H16V13Z"
+                                                        fill="red"
+                                                    />
+                                                </Svg>
+                                                <Text style={{ fontSize: 16, fontWeight: "bold", color: "red" }}>Kijelentkezés</Text>
+                                            </View>
+                                        </Pressable>
                                     </View>
-                                </Pressable>
+                                </View>
+                                <View style={{ gap: 18 }}>
+                                    <Text style={{ fontSize: 20, fontWeight: "bold", color: "#202020" }}>Biztonság</Text>
+                                    <View style={{ gap: 18 }}>
+                                        <Pressable>
+                                            <View style={{ backgroundColor: "white", width: "100%", paddingVertical: 16, paddingHorizontal: 16, borderRadius: 6, flexDirection: "row", alignItems: "center", gap: 16 }}>
+                                                <Svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                    <Path
+                                                        d="M7 7.13193V6.61204C7 4.46614 7 3.3932 7.6896 2.79511C8.37919 2.19703 9.44136 2.34877 11.5657 2.65224L15.8485 3.26408C18.3047 3.61495 19.5327 3.79039 20.2664 4.63628C21 5.48217 21 6.72271 21 9.20377V14.7962C21 17.2773 21 18.5178 20.2664 19.3637C19.5327 20.2096 18.3047 20.385 15.8485 20.7359L11.5657 21.3478C9.44136 21.6512 8.37919 21.803 7.6896 21.2049C7 20.6068 7 19.5339 7 17.388V17.066"
+                                                        stroke="red"
+                                                        stroke-width="2"
+                                                    />
+                                                    <Path
+                                                        d="M16 12L16.7809 11.3753L17.2806 12L16.7809 12.6247L16 12ZM4 13C3.44771 13 3 12.5523 3 12C3 11.4477 3.44771 11 4 11V13ZM12.7809 6.3753L16.7809 11.3753L15.2191 12.6247L11.2191 7.6247L12.7809 6.3753ZM16.7809 12.6247L12.7809 17.6247L11.2191 16.3753L15.2191 11.3753L16.7809 12.6247ZM16 13H4V11H16V13Z"
+                                                        fill="red"
+                                                    />
+                                                </Svg>
+                                                <Text style={{ fontSize: 16, fontWeight: "bold", color: "red" }}>Kijelentkezés</Text>
+                                            </View>
+                                        </Pressable>
+                                        <Pressable>
+                                            <View style={{ backgroundColor: "white", width: "100%", paddingVertical: 16, paddingHorizontal: 16, borderRadius: 6, flexDirection: "row", alignItems: "center", gap: 16 }}>
+                                                <Svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                    <Path
+                                                        d="M7 7.13193V6.61204C7 4.46614 7 3.3932 7.6896 2.79511C8.37919 2.19703 9.44136 2.34877 11.5657 2.65224L15.8485 3.26408C18.3047 3.61495 19.5327 3.79039 20.2664 4.63628C21 5.48217 21 6.72271 21 9.20377V14.7962C21 17.2773 21 18.5178 20.2664 19.3637C19.5327 20.2096 18.3047 20.385 15.8485 20.7359L11.5657 21.3478C9.44136 21.6512 8.37919 21.803 7.6896 21.2049C7 20.6068 7 19.5339 7 17.388V17.066"
+                                                        stroke="red"
+                                                        stroke-width="2"
+                                                    />
+                                                    <Path
+                                                        d="M16 12L16.7809 11.3753L17.2806 12L16.7809 12.6247L16 12ZM4 13C3.44771 13 3 12.5523 3 12C3 11.4477 3.44771 11 4 11V13ZM12.7809 6.3753L16.7809 11.3753L15.2191 12.6247L11.2191 7.6247L12.7809 6.3753ZM16.7809 12.6247L12.7809 17.6247L11.2191 16.3753L15.2191 11.3753L16.7809 12.6247ZM16 13H4V11H16V13Z"
+                                                        fill="red"
+                                                    />
+                                                </Svg>
+                                                <Text style={{ fontSize: 16, fontWeight: "bold", color: "red" }}>Kijelentkezés</Text>
+                                            </View>
+                                        </Pressable>
+                                    </View>
+                                </View>
                             </View>
                         </SafeAreaView>
                     </View>

1e3ee1c62f8c669775a52483ae0387be369eecea
kiralysanyi <kiralysandor1986@gmail.com>
2024-01-27 14:58:52 +0100

    - Some changes

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import AppStack from './navigation/AppStack';
 import { StatusBar } from 'expo-status-bar';
+import TrackPlayer from "react-native-track-player";
+
+TrackPlayer.registerPlaybackService(() => require('./service'));
 
 export default function App() {
   return (

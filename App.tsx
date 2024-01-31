d067ad2624b71e8a4e2f062f7d4204fc06be3ee2
codevolume <szilvesztermarinka4@gmail.com>
2024-01-31 19:13:33 +0100

    expo to reactnative

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import AppStack from './navigation/AppStack';
-import { StatusBar } from 'expo-status-bar';
 import TrackPlayer from "react-native-track-player";
 
 TrackPlayer.registerPlaybackService(() => require('./service'));
